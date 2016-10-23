bff685a90951a760d24b429eda1a1e88687b9c1b
Nick Duskey <nicholasduskey@gmail.com>
2016-10-23 19:03:54 -0400

    Added dashboard component

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="dashboard">
+    <h1 class="page-header">Dashboard</h1>
+    <div class="row placeholders">
+      <div class="col-xs-6 col-sm-3 placeholder">
+        <img
+          class="img-responsive"
+          src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
+          alt="Generic placeholder thumbnail"
+          width="200"
+          height="200">
+        <h4>Label</h4>
+        <span class="text-muted">Something else</span>
+      </div>
+      <div class="col-xs-6 col-sm-3 placeholder">
+        <img
+          class="img-responsive"
+          src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
+          alt="Generic placeholder thumbnail"
+          width="200"
+          height="200">
+        <h4>Label</h4>
+        <span class="text-muted">Something else</span>
+      </div>
+      <div class="col-xs-6 col-sm-3 placeholder">
+        <img
+          class="img-responsive"
+          src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
+          alt="Generic placeholder thumbnail"
+          width="200"
+          height="200">
+        <h4>Label</h4>
+        <span class="text-muted">Something else</span>
+      </div>
+      <div class="col-xs-6 col-sm-3 placeholder">
+        <img
+          class="img-responsive"
+          src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
+          alt="Generic placeholder thumbnail"
+          width="200"
+          height="200">
+        <h4>Label</h4>
+        <span class="text-muted">Something else</span>
+      </div>
+    </div>
+    <h2 class="sub-header">Section Title</h2>
+    <div class="table-responsive">
+      <thead>
+        <tr>
+          <th>
+            #
+          </th>
+          <th>
+            Header
+          </th>
+          <th>
+            Header
+          </th>
+          <th>
+            Header
+          </th>
+          <th>
+            Header
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            1,001
+          </td>
+          <td>
+            lorem
+          </td>
+          <td>
+            ipsum
+          </td>
+          <td>
+            dolor
+          </td>
+          <td>
+            sit
+          </td>
+        </tr>
+      </tbody>
+    </div>
+  </div>
+</template>
